36a1322d3cb999df3949d287c9a4c365e5c1c320
Pablo Ruiz Garcia <pablo.ruiz@gmail.com>
2012-06-18 05:49:04 +0200

    Added helper classes to allow reading/writing PNG files to/from streams.

---
+++ Mono.CairoWarp/CairoStreamReader.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using Cairo;
+
+namespace CairoWarp
+{
+	public class CairoStreamReader
+	{
+		private static Type[] _ctorTypes = new[] { typeof(IntPtr), typeof(bool) };
+		private static ConstructorInfo _ctor = typeof(ImageSurface).GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, _ctorTypes, null);
+
+		private BinaryReader _reader;
+		private byte[] _data;
+		private int offset;
+
+		private CairoStreamReader(byte[] data)
+		{
+			if (data == null) throw new ArgumentNullException("data");
+
+			_data = data;
+		}
+
+		private CairoStreamReader(BinaryReader reader)
+		{
+			if (reader == null) throw new ArgumentNullException("reader");
+
+			_reader = reader;
+		}
+
+		private Status do_read(IntPtr closure, IntPtr outbuf, uint length)
+		{
+			//Console.WriteLine("closure = {0} - lenght: {1}", closure, length);
+
+			if (_data != null)
+			{
+				Marshal.Copy(_data, offset, outbuf, (int)length);
+				offset += (int)length;
+			}
+			else
+			{
+				var tmp = _reader.ReadBytes((int)length);
+				Marshal.Copy(tmp, 0, outbuf, (int)length);
+			}
+
+			return Status.Success;
+		}
+
+		public static ImageSurface ImageSurfaceFromPng(byte[] data)
+		{
+			var obj = new CairoStreamReader(data);
+			var fn = new cairo_read_func_t(obj.do_read);
+			var surface = cairo_image_surface_create_from_png_stream(fn, IntPtr.Zero);
+			return (ImageSurface)_ctor.Invoke(new object[] { surface, false });
+		}
+
+		public static ImageSurface ImageSurfaceFromPng(BinaryReader reader)
+		{
+			var obj = new CairoStreamReader(reader);
+			var fn = new cairo_read_func_t(obj.do_read);
+			var surface = cairo_image_surface_create_from_png_stream(fn, IntPtr.Zero);
+			return (ImageSurface)_ctor.Invoke(new object[] { surface, false });
+		}
+
+		public static ImageSurface ImageSurfaceFromPng(Stream stream)
+		{
+			return ImageSurfaceFromPng(new BinaryReader(stream));
+		}
+
+		#region Native Calls
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+		private delegate Status cairo_read_func_t(IntPtr closure, IntPtr data, uint length);
+
+		[DllImport("libcairo-2.dll")]
+		private static extern IntPtr cairo_image_surface_create_from_png_stream(
+			[MarshalAs(UnmanagedType.FunctionPtr)] cairo_read_func_t cb,
+			IntPtr closure
+		);
+		#endregion
+	}
+}
---
+++ Mono.CairoWarp/CairoStreamWriter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using Cairo;
+
+namespace CairoWarp
+{
+	public class CairoStreamWriter
+	{
+		private static Type[] _ctorTypes = new[] { typeof(IntPtr), typeof(bool) };
+		private static ConstructorInfo _ctor = typeof(ImageSurface).GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, _ctorTypes, null);
+
+		private BinaryWriter _writer;
+
+		private CairoStreamWriter(Stream stream)
+			: this(new BinaryWriter(stream))
+		{
+		}
+
+		private CairoStreamWriter(BinaryWriter writer)
+		{
+			if (writer == null) throw new ArgumentNullException("writer");
+
+			_writer = writer;
+		}
+
+		private Status do_write(IntPtr closure, IntPtr inbuf, uint length)
+		{
+			//Console.WriteLine("closure = {0} - lenght: {1}", closure, length);
+
+
+			var tmp = new byte[(int)length];
+			Marshal.Copy(inbuf, tmp, 0, (int)length);
+			_writer.Write(tmp);
+
+			return Status.Success;
+		}
+
+		public static void SurfaceToPngStream(Surface surface, BinaryWriter writer)
+		{
+			var obj = new CairoStreamWriter(writer);
+			var fn = new cairo_write_func_t(obj.do_write);
+			var status = cairo_surface_write_to_png_stream(surface.Handle, fn, IntPtr.Zero);
+
+			if (status != Status.Success)
+				throw new InvalidOperationException("Status: " + status);
+		}
+
+		public static void SurfaceToPngStream(Surface surface, Stream stream)
+		{
+			SurfaceToPngStream(surface, new BinaryWriter(stream));
+		}
+
+		public static byte[] SurfaceToPngBytes(Surface surface)
+		{
+			using (var ms = new MemoryStream())
+			{
+				SurfaceToPngStream(surface, ms);
+				return ms.ToArray();
+			}
+		}
+
+		#region Native Calls
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+		private delegate Status cairo_write_func_t(IntPtr closure, IntPtr data, uint length);
+
+		[DllImport("libcairo-2.dll")]
+		private static extern Status cairo_surface_write_to_png_stream (
+			IntPtr surface,
+			[MarshalAs(UnmanagedType.FunctionPtr)] cairo_write_func_t cb,
+			IntPtr closure
+		);
+		#endregion
+	}
+}
